3dec632e939cc0b6557611f43457a33e3d2a7d33
David Gudeman <davidgudeman@davids-mbp.gateway.pace.com>
2015-06-15 09:32:44 -0700

    first commit

---
+++ CS21Aassignment02.py
@@ -0,0 +1,48 @@
+# -----------------------------------------------------------------------------
+# Name:        translate
+# Purpose:     assignment # 2
+#
+# Author:
+# Date:
+# -----------------------------------------------------------------------------
+"""
+Enter your docstring with a one-line overview here
+
+and a more detailed description here.
+"""
+
+def starts_with_vowel(word):
+    """
+    return True if the word starts with a vowel and False otherwise
+    """
+def encode(word):
+    """
+    # translate a single word to the secret language
+    # call starts with vowel to decide which pattern to follow
+    """
+def translate(text):
+    """
+    # translate the whole text to the secret language
+    # call encode to translate individual words in text
+    """
+def get_input():
+    """
+    Obtain the input from the user
+
+    """
+    # prompt the user for input until they enter a non-empty string
+    # return the string entered by the user
+    #
+messagein = input('Please enter your message')
+messagesplit = messagein.split()
+print(messagesplit)
+
+def main():
+
+    """# get input and save it in a variable
+    # translate the saved input by calling translate - save result
+    # print the result
+    """
+
+if __name__ == '__main__':
+    main()
